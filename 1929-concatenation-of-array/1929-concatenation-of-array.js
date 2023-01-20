80fd28b2e15bec42d704f537f20b5021f356486c
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2023-01-20 13:11:33 -0500

    Time: 75 ms (91.46%), Space: 44.8 MB (84.63%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let result = [];
+    for(let i=0; i<nums.length; i++){
+        result[i]= nums[i];
+        result[i+nums.length] = nums[i]
+    }
+    return result;
+};
