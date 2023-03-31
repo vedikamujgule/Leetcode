0550acf1609c398be68895fe8c6b9de3706d7d49
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2023-03-31 17:18:13 -0400

    Time: 69 ms (52.88%), Space: 44.4 MB (23.38%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -3,26 +3,14 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-    // count = 0;
-    // s=s.split("")
-    // const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
-    // for(let i=0; i<s.length /2; i++){
-    //     if(vowels.has(s[i]))
-    //         count ++
-    //     if(vowels.has(s[s.length/2]+i))
-    //         count --
-    // }
-    // return count ===0
-    
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
-  let count = 0;
-  for (let i = 0; i < s.length / 2; i++) {
-    if (vowels.has(s[i])) {
-      count++;
+    count = 0;
+    s=s.split("")
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+    for(let i=0; i<s.length /2; i++){
+        if(vowels.has(s[i]))
+            count ++
+        if(vowels.has(s[s.length/2 +i]))
+            count --
     }
-    if (vowels.has(s[s.length / 2 + i])) {
-      count--;
-    }
-  }
-  return count === 0;
+    return count ===0
 };
