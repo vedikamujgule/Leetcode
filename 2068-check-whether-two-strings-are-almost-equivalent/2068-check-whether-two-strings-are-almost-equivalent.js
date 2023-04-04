65c9bd742f66472bb15586e7b15215f5366f944b
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2023-04-04 16:08:41 -0400

    Time: 65 ms (83.83%), Space: 44.6 MB (56.29%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var checkAlmostEquivalent = function(word1, word2) {
+    const hash = {};
+    let output = true;
+
+    [...word1].map( char => {
+            hash[char] = hash[char] + 1 || 1;
+        }
+    );
+
+    [...word2].map(char => {
+        if(hash[char]) {
+            hash[char] = hash[char] - 1;
+        } else {
+            hash[char] = - 1;
+        }
+    });
+    
+    for(let char in hash) {
+        Math.abs(hash[char]) > 3 ? output = false : output; 
+    }
+
+    return output;
+};
