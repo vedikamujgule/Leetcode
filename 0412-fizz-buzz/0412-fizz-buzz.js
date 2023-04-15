f34f8f1305e227683769a046cc19dc9e427820eb
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2023-04-15 16:31:16 -0400

    Time: 76 ms (18.80%), Space: 44.9 MB (8.46%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    const answer = [];
+    for(let i=1;i<n+1;i++){
+        if (i % 3 === 0 && i % 5 === 0)
+            answer.push("FizzBuzz")
+        else if (i%5 ==0)
+            answer.push("Buzz")
+        else if (i%3 ==0)
+            answer.push("Fizz")
+        else 
+            answer.push(i.toString())
+    }
+    return answer;
+};
