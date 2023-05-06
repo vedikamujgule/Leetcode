d69e54eb9d054e694ce926ae1a96d7a63012d56f
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2023-05-06 10:19:06 -0400

    Time: 77 ms (58.40%), Space: 43.7 MB (17.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,14 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var nums=[2,7,11,15]
-var target = 9
 var twoSum = function(nums, target) {
+    let map = new Map();
     for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j] == target)
-            return[i,j];
+        if(map.has(target-nums[i])){
+            return [i, map.get(target-nums[i])]
         }
+        map.set(nums[i], i)
     }
 };
-
